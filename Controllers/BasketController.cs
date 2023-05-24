98c146ce4c1886485a0e63b2e9c5f76992e42de7
cosqun5 <87088111+cosqun5@users.noreply.github.com>
2023-05-24 12:44:21 +0400

    backet creat

--- Controllers/BasketController.cs
+++ Controllers/BasketController.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Newtonsoft.Json;
+using WebFrontToBack.DAL;
 using WebFrontToBack.ViewModel;
 
 namespace WebFrontToBack.Controllers
@@ -7,10 +9,56 @@ namespace WebFrontToBack.Controllers
     public class BasketController : Controller
     {
         private const string COOKIES_BASKET = "basketVM";
+        private readonly AppDbContext _appDbContext;
+        public BasketController(AppDbContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+        }
         public IActionResult Index()
         {
-            List<BasketVM> basketVMs = JsonConvert.DeserializeObject<List<BasketVM>>(Request.Cookies[COOKIES_BASKET]);
-            return Json(basketVMs);
+			List<BasketItemVM> basketItemVMs = new List<BasketItemVM>();
+			List<BasketVM> basketVMs = JsonConvert.DeserializeObject<List<BasketVM>>(Request.Cookies[COOKIES_BASKET]);
+			foreach (BasketVM item in basketVMs)
+			{
+				BasketItemVM basketItemVM = _appDbContext.Services
+												.Where(s => !s.IsDeleted && s.Id == item.ServiceId)
+												.Include(s => s.Category)
+												.Include(s => s.ServiceImages)
+												.Select(s => new BasketItemVM
+												{
+													Name = s.Name,
+													Id = s.Id,
+													CategoryName = s.Category.Name,
+													IsDeleted = s.IsDeleted,
+													Price = s.Price,
+													ServiceCount = item.Count,
+													ImagePath = s.ServiceImages.FirstOrDefault(i => i.IsActive).Path
+												}).FirstOrDefault();
+				basketItemVMs.Add(basketItemVM);
+				//List<BasketItemVM> basketItemVMs = new List<BasketItemVM>();
+				//List<BasketVM> basketVMs = JsonConvert.DeserializeObject<List<BasketVM>>(Request.Cookies[COOKIES_BASKET]);
+				//foreach (BasketVM item in basketVMs)
+				//{
+				//    BasketItemVM basketItemVM = _appDbContext.Services
+				//     .Where(s => !s.IsDeleted && s.Id == item.ServiceId)
+				//     .Include(s => s.Category)
+				//     .Include(s => s.ServiceImages)
+				//     .Select(s => new BasketItemVM
+				//     {
+				//         Name = s.Name,
+				//         Id = s.Id,
+				//         CategoryName = s.Category.Name,
+				//         IsDeleted = s.IsDeleted,
+				//         Price = s.Price,
+				//         ServiceCount = item.Count,
+				//         ImagePath = s.ServiceImages.FirstOrDefault(i => i.IsActive).Path
+				//     }).FirstOrDefault();
+				//    basketItemVMs.Add(basketItemVM);
+
+
+
+			}
+            return View(basketItemVMs);
         }
         public IActionResult AddBasket(int id)
         {
